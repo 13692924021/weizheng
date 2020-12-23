77d2407cb99930f93e8ff11315561974323541ee
weizheng <55438322+13692924021@users.noreply.github.com>
2020-12-23 17:57:34 +0800

    1223

--- vue3.0/src/main.ts
+++ vue3.0/src/main.ts
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
+
+//shallowRef , shallowReactive  只监听第一层数据，不递归监听
+//toRaw  获取reactive数据的原始数据， 修改不更新视图
+//markRaw 用markRaw返回的对象传给reactive不贵触发视图更新
+//toRef(obj,'key')  关原始数据，修改的同时会修改原始数据， 但不触发视图更新
+//toRefs(obj) 
+// customRef((track,trigger) => {     //有点类似computed
+//     return {
+//         get () {
+//             track()      //告诉Vue这个数据需要追踪
+//             return 123
+//         },
+//         set () {
+//             trigger()  //书法视图更新
+//         }
+//     }
+// })
+
 import App from './App.vue'
 const app:any = createApp(App)
 
