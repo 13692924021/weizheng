cfec0c89704e2bb779f5d359ed27a4066636ca9d
weizheng <972307875@qq.com>
2021-03-07 14:32:47 +0800

    按钮hover动画

--- vue2.0模板(组件库)/src/view/css/css.vue
+++ vue2.0模板(组件库)/src/view/css/css.vue
@@ -1,74 +1,21 @@
 <style lang="less" scoped>
-.borderMove {
-    >i:first-child {
-        width: 100%;
-        height: 2px;
-        left: -100%;
-        top: 0;
-    }
-    &:hover > i:first-child {
-        left: 0 !important;
-    }
-
-    >i:nth-of-type(2) {
-        width: 2px;
-        height: 100%;
-        right: 0%;
-        top: -100%;
-    }
-    &:hover > i:nth-of-type(2) {
-        top: 0 !important;
-    }
-    >i:nth-of-type(3) {
-        height: 2px;
-        width: 100%;
-        left: 100%;
-        bottom: 0%;
-    }
-    &:hover > i:nth-of-type(3) {
-        left: 0 !important;
-    }
-
-    >i:nth-of-type(4) {
-        width: 2px;
-        height: 100%;
-        left: 0%;
-        top: 100%;
-    }
-    &:hover > i:nth-of-type(4) {
-        top: 0% !important;
-    }
-    &:hover {
-        box-shadow: 0 0 10px #6495ED;
-    }
-
-
-    position: relative;
-    overflow:hidden;
-    transition: 0.3s;
-    outline: none;
-    border: none;
-    display: inline-block;
-    background: cornflowerblue;
-    padding: 0.5em 2em;
-    color: #FFF;
-    cursor: pointer;
-
-    >i {
-        position: absolute;
-        background: #011604;
-        transition: 0.3s;
-    }
+.cssWrap {
+    // background: #000;
+    min-height: 100%;
+    padding: 30px;
 }
+
 
 </style>
 
 <template>
-   <div>
+   <div class="cssWrap">
        <button class="borderMove op">
            <i></i><i></i><i></i><i></i>
            <span>Button</span>
        </button>
+        <br>
+        <br>
            
    </div>
 </template>
